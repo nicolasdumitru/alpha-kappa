12a3c5f74f961b27a38738512c408c11aa1cfdc9
nicolasdumitru <nicolas@ndumitru.com>
2025-04-27 23:20:10 +0300

    Update flake.nix with streamlined package handling

--- flake.nix
+++ flake.nix
@@ -2,19 +2,16 @@
   description = "Development environment for Alpha-Kappa";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # You can pin a specific revision if needed
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-
   outputs =
     {
-      self,
       nixpkgs,
       ...
     }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-
+      pkgs = nixpkgs.legacyPackages."${system}";
     in
     {
       devShell."${system}" = pkgs.mkShell {
@@ -24,15 +21,10 @@
               numpy
               scipy
               matplotlib
-
               flask
               flask-cors
-
-              # python-lsp-server
             ]
           ))
-          # jetbrains.pycharm-professional
-          # pyright
           black
         ];
       };
