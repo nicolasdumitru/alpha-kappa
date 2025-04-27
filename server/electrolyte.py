2027dddab52c7a12eb53236e622d2d425d9e4d39
nicolasdumitru <nicolas@ndumitru.com>
2025-04-27 18:54:24 +0300

    Update electrolyte.py
    Refactor the constructor for `Electrolyte`.
    Add `Electrolyte` methods: `display_attributes` and `to_dict`.

--- server/electrolyte.py
+++ server/electrolyte.py
@@ -1,53 +1,77 @@
 import numpy as np
+from enum import Enum
+
+
+class ElectrolyteType(Enum):
+    Acid = 0
+    Base = 1
+    Salt = 2
+
+
+Acid = ElectrolyteType.Acid
+Base = ElectrolyteType.Base
+Salt = ElectrolyteType.Salt
 
 
 class Electrolyte:
     def __init__(self, data):
         # Parse data from the dictionary
-        self.mode = data.get("mode")
-        self.is_acid = True if data.get("type") == "acid" else False
+        self.mode = data.get("mode")  # WARNING: might be deprecated in the future
+
+        self.type = (
+            Acid
+            if data.get("type") == "acid"
+            else Base if data.get("type") == "base" else Salt
+        )
         self.is_strong = True if data.get("strength") == "strong" else False
         self.name = data.get("name")
         self.tcid = np.float64(data.get("theoreticalConductivityAtInfiniteDilution"))
-        self.length = np.int32(data.get("length"))
         self.concentrations = np.array(data.get("concentrations", []), dtype=np.float64)
         self.temperatures = np.array(data.get("temperatures", []), dtype=np.float64)
 
         # initialize the graphical conductivity at infinite dilution
         self.gcid = np.float64(0)
 
-        if self.mode == "concentrations":
-            tmp = data.get("storedConductivitiesForConcentrations", [])
-        else:
-            tmp = data.get("storedConductivitiesForTemperatures", [])
-
-        # TODO (Fix): Shrink arrays to fit data. Random zeros here and there will mess up the regression results.
-        for i in range(self.length):  # Initialize storedC to 0.0 if None
-            if tmp[i] is None:
-                tmp[i] = 0.0
-
-        self.conductivities = np.array(tmp, dtype=np.float64)
+        self.conductivities = np.array(data.get("conductivities", []), dtype=np.float64)
 
         # initialize molar conductivities:
         self.mc = 1000 * self.conductivities / self.concentrations
 
         # initialize dissociation coeficients:
-        self.alpha = (
-            np.ones(self.length, dtype=np.float64)
-            if self.is_strong
-            else self.mc / self.tcid
-        )
+        self.alpha = self.mc / self.tcid
 
-        self.kd = (
-            (self.mc**2) * self.concentrations / (self.tcid * (self.tcid - self.mc))
-            if self.is_strong
-            else (self.alpha**2) * self.concentrations / (1 - self.alpha)
-        )
+        self.kd = self.mc**2 * self.concentrations / (self.tcid * (self.tcid - self.mc))
+
         # initialize pH & pOH
-        # TODO: fix
-        if self.is_acid:
-            self.pH = -1 * np.log10(self.alpha * self.concentrations)
-            self.pOH = 14 - self.pH
-        else:
-            self.pOH = -1 * np.log10(self.concentrations)
-            self.pH = 14 - self.pOH
+        if self.type == Acid:
+            self.ph = -1 * np.log10(self.alpha * self.concentrations)
+            self.poh = 14 - self.ph
+        elif self.type == Base:
+            self.poh = -1 * np.log10(self.concentrations)
+            self.ph = 14 - self.poh
+        else:  # Salt
+            self.ph = np.array([0.0], dtype=np.float64)
+            self.poh = np.array([0.0], dtype=np.float64)
+
+    def display_attributes(self):
+        print("mode:", self.mode)
+        print("name:", self.name)
+        print("type:", self.type)
+        print("is strong:", self.is_strong)
+        print("Stored conductivities:", self.conductivities)
+        print("Molar conductivities:", self.mc)
+        print("Alpha:", self.alpha)
+        print("Kd:", self.kd)
+        print("pH:", self.ph)
+        print("pOH:", self.poh)
+
+    def to_dict(self):
+        return {
+            "mc": self.mc.tolist(),
+            "alpha": self.alpha.tolist(),
+            "kd": self.kd.tolist(),
+            "pH": self.ph.tolist(),
+            "pOH": self.poh.tolist(),
+            "A": self.a,
+            "B": self.b,
+        }
