c6ae98bd067ea747b6f29685074bc44078d7983a
nicolasdumitru <nicolas@ndumitru.com>
2025-05-01 14:32:59 +0300

    Add regressions and improve numerical stability

--- server/electrolyte.py
+++ server/electrolyte.py
@@ -1,5 +1,38 @@
-import numpy as np
 from enum import Enum
+
+import numpy as np
+from numpy.typing import NDArray
+import scipy
+
+
+def linear_regression(x, y) -> tuple[np.float64, np.float64, np.float64]:
+    res = scipy.stats.linregress(x, y)
+    return (
+        np.float64(res.slope),
+        np.float64(res.intercept),
+        np.float64(res.rvalue) ** 2,  # R^2
+    )
+
+
+def exponential_regression(
+    x: NDArray[np.float64], y: NDArray[np.float64]
+) -> tuple[np.float64, np.float64, np.float64]:
+    def exp_model(x: NDArray[np.float64], a: float, b: float) -> NDArray[np.float64]:
+        return a * np.exp(b * x)
+
+    params, _ = scipy.optimize.curve_fit(exp_model, x, y)
+
+    amplitude = np.float64(params[0])
+    rate = np.float64(params[1])
+    print(f"amplitude: {amplitude}\nrate: {rate}") # debug print
+
+    y_pred = exp_model(x, amplitude, rate)
+
+    rss = np.sum(np.square(y - y_pred))
+    tss = np.sum(np.square(y - np.mean(y)))
+    r_squared = np.float64(1 - rss / tss)
+
+    return amplitude, rate, r_squared
 
 
 class ElectrolyteType(Enum):
@@ -16,48 +49,41 @@ Salt = ElectrolyteType.Salt
 class Electrolyte:
     def __init__(self, data):
         # Parse data from the dictionary
-        # WARNING: might be deprecated in the future
-        self.mode = data.get("mode")
+        self.mode = data.get("mode")  # WARNING: might be deprecated in the future
 
         self.type = (
             Acid
             if data.get("type") == "acid"
             else Base if data.get("type") == "base" else Salt
         )
-        self.strong = True if data.get("strength") == "strong" else False
+        self.is_strong = True if data.get("strength") == "strong" else False
         self.name = data.get("name")
-        self.tcid = np.float64(
-            data.get("theoreticalConductivityAtInfiniteDilution"))
-        self.concentrations = np.array(
-            data.get("concentrations", []), dtype=np.float64)
+        self.tcid = np.float64(data.get("theoreticalConductivityAtInfiniteDilution"))
+        self.concentrations = np.array(data.get("concentrations", []), dtype=np.float64)
 
-        # initialize the graphical conductivity at infinite dilution
-        self.gcid = np.float64(0)
-
-        # initialize the elements for the mathematical regression y=A*x+B (strong electrolyte) or y=A*e^(B*x) (weak electrolyte)
-        # we need these two inside the class for the toDict() method, but they are calculated
-        self.A = 0.0
-        self.B = 0.0
-
-        self.conductivities = np.array(
-            data.get("conductivities", []), dtype=np.float64)
+        self.conductivities = np.array(data.get("conductivities", []), dtype=np.float64)
 
         # calculate molar conductivities:
         self.mc = 1000 * self.conductivities / self.concentrations
 
         # calculate dissociation coeficients:
-        if not self.strong:
-            self.alpha = self.mc / self.tcid
-        else: # strong
-            self.alpha = np.ones(len(self.concentrations), dtype=np.float64) # make them ones for strong electrolytes
+        self.alpha = (
+            np.ones(len(self.concentrations), dtype=np.float64)
+            if self.is_strong
+            else self.mc / self.tcid
+        )
 
-        if not self.strong:
-            self.kd = self.mc**2 * self.concentrations / \
-                (self.tcid * (self.tcid - self.mc))
-        else: # strong
-            self.kd = np.zeros(len(self.concentrations), dtype=np.float64) #just make them all zeros, we won't display Kd for strong electrolytes
+        self.kd = (
+            np.zeros(len(self.concentrations), dtype=np.float64)
+            if self.is_strong
+            else (
+                np.square(self.mc)
+                * self.concentrations
+                / (self.tcid * (self.tcid - self.mc))
+            )
+        )
 
-        # calculate pH & pOH
+        # initialize pH & pOH
         if self.type == Acid:
             self.ph = -1 * np.log10(self.alpha * self.concentrations)
             self.poh = 14 - self.ph
@@ -68,11 +94,20 @@ class Electrolyte:
             self.ph = np.zeros(len(self.concentrations), dtype=np.float64)
             self.poh = np.zeros(len(self.concentrations), dtype=np.float64)
 
-    def displayAttributes(self):
+        # Regression:
+        # strong electrolytes => linear regression
+        # weak electrolytes => exponential regression
+        x = np.sqrt(self.concentrations)  # x axis data
+        y = self.mc  # y axis data
+        self.a, self.b, self.r_squared = (
+            linear_regression(x, y) if self.is_strong else exponential_regression(x, y)
+        )
+
+    def display_attributes(self):
         print("mode:", self.mode)
         print("name:", self.name)
         print("type:", self.type)
-        print("is strong:", self.strong)
+        print("is strong:", self.is_strong)
         print("Stored conductivities:", self.conductivities)
         print("Molar conductivities:", self.mc)
         print("Alpha:", self.alpha)
@@ -80,13 +115,14 @@ class Electrolyte:
         print("pH:", self.ph)
         print("pOH:", self.poh)
 
-    def toDict(self):
+    def to_dict(self):
         return {
-            "mc": self.mc.tolist(),
             "alpha": self.alpha.tolist(),
+            "mc": self.mc.tolist(),
             "kd": self.kd.tolist(),
             "pH": self.ph.tolist(),
             "pOH": self.poh.tolist(),
-            "A": self.A,
-            "B": self.B,
+            "A": self.a,
+            "B": self.b,
+            "R^2": self.r_squared,
         }
